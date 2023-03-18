386db8c8a8d37ad5bf44bb9b3b75d2bdd807863c
monica <monibrave1@gmail.com>
2023-03-18 02:20:07 -0300

    all is ready with stats

--- assets/js/my_functions.js
+++ assets/js/my_functions.js
@@ -1,13 +1,13 @@
 //function that generate cards
 //do cards 2
-let do_cards2 = (array,container) =>{
-    if(array.length == 0){
+let do_cards2 = (array, container) => {
+    if (array.length == 0) {
         //console.log(container_card);
-        return container.innerHTML = `<h5 class="col d-flex flex-column justify-content-center align-items-center" id="message">It was not found "${input.value}" between events!</h5>`;  
-    }else{
+        return container.innerHTML = `<h5 class="col d-flex flex-column justify-content-center align-items-center" id="message">It was not found "${input.value}" between events!</h5>`;
+    } else {
         let structure_card = "";
-    array.forEach((el) => {
-        structure_card += `
+        array.forEach((el) => {
+            structure_card += `
         <div class="card" data-id ="${el.category}">
             <img src="${el.image}" class="card-img-top" alt="${el.name}">
             <div class="card-body">
@@ -19,20 +19,35 @@ let do_cards2 = (array,container) =>{
             </div>
         </div>
         `;
-    });
-    return structure_card;
+        });
+        return structure_card;
     }
-    
+
+}
+
+//paint table
+let paint_table_data = (container,array)=>{
+    let structure=""
+    array.forEach((el)=>{
+        structure += `
+        <tr class="text-center">
+            <td>${el.category}</td>
+            <td>$${el.revenues}</td>
+            <td >${el.porcentage}%</td>
+        </tr>
+        `
+    })
+    container.innerHTML= structure;
 }
 
 // past events filter
-let filter_events_past = (actual_date,array)=>{
-    return array.filter(el=> actual_date>el.date);
+let filter_events_past = (actual_date, array) => {
+    return array.filter(el => actual_date > el.date);
 }
 
 //future events filter
-let filter_future_events = (actual_date,array)=>{
-    return array.filter(el=> actual_date<el.date);
+let filter_future_events = (actual_date, array) => {
+    return array.filter(el => actual_date < el.date);
 }
 
 //function to do categories dinamicaly
@@ -59,20 +74,20 @@ let filter_unic_property = (array) => {
 //filtered functions for input and checks
 //funcion filtro checks
 
-let filtered_from_input = (array,string) =>{
+let filtered_from_input = (array, string) => {
     filtered_array = array.filter(elemento => elemento.name.toLowerCase().includes(string.toLowerCase()))
     return filtered_array
 }
 
-let filtered_from_categories= (array)=>{
+let filtered_from_categories = (array) => {
     let checkboxes = document.querySelectorAll("input[type='checkbox']")
     //console.log(checkboxes) 
     let arrayChecks = Array.from(checkboxes)
     let checksCheckeados = arrayChecks.filter(check => check.checked)
-    if(checksCheckeados.length == 0){
+    if (checksCheckeados.length == 0) {
         return array
     }
-    
+
     let categories = checksCheckeados.map(check => check.value)
     let filtered_array = array.filter(elemento => categories.includes(elemento.category))
     //console.log(filtered_array)
@@ -82,12 +97,137 @@ let filtered_from_categories= (array)=>{
 }
 
 //convinando filtros
-let combined_filter = (e,array,container)=>{
+let combined_filter = (e, array, container) => {
     e.preventDefault();
-    let first_filter = filtered_from_input(array,input.value);
+    let first_filter = filtered_from_input(array, input.value);
     let second_filter = filtered_from_categories(first_filter);
-    container.innerHTML = do_cards2(second_filter,container)
+    container.innerHTML = do_cards2(second_filter, container)
 }
+//function to obtein high event porcentage attendance
+let high_event_attendance = (array) => {
+    let high_event = array[0]
+    array.forEach((el) => {
+        if ((el.assistance / el.capacity * 100) > (high_event.assistance / high_event.capacity * 100)) {
+            high_event = el;
+        }
+
+    })
+    return high_event;
+}
+//function to obtein low event porcentage attendance
+let low_event_attendance = (array) => {
+    let low_event = array[0]
+    array.forEach((el) => {
+        if ((el.assistance / el.capacity * 100) < (low_event.assistance / low_event.capacity * 100)) {
+            low_event = el;
+        }
+
+    })
+    return low_event;
+}
+
+let porcentage_attendance = (assistance, capacity) => ((assistance / capacity) * 100).toFixed(2);
+
+//function to obtein event with high capacity
+let high_event_capacity = (array) => {
+    let high_capacity = array[0]
+    array.forEach((el) => {
+        if ((el.capacity) > (high_capacity.capacity)) {
+            high_capacity = el;
+        }
+
+    })
+    return high_capacity;
+}
+//earnings by category
+let category_revenues = (array) => {
+    return array.reduce((acc, evento) => {
+        if (acc[evento.category]) {
+            acc[evento.category] += evento.price * evento.estimate;
+        } else {
+            acc[evento.category] = evento.price * evento.estimate;
+        }
+        return acc;
+    }, {});
+}
+//estimated attendance by category up
+let category_attendance = (array) => {
+    return array.reduce((acc, e) => {
+        if (acc[e.category]) {
+            acc[e.category].estimate += e.estimate;
+            acc[e.category].capacity += e.capacity;
+        } else {
+            acc[e.category] = {
+                estimate: e.estimate,
+                capacity: e.capacity,
+            };
+        }
+        return acc;
+    }, {})
+    
+}
+
+let porcentage_category_attendance = (array_category)=>{
+    let porcentage_attendance = {}
+    for(let el in array_category){
+        porcentage = (array_category[el].estimate/array_category[el].capacity * 100).toFixed(2)
+        porcentage_attendance[el] = parseFloat(porcentage);
+    }
+    return porcentage_attendance;
+}
+//functions to past events
+
+//earnings by category
+let past_category_revenues = (array) => {
+    return array.reduce((acc, evento) => {
+        if (acc[evento.category]) {
+            acc[evento.category] += evento.price * evento.assistance;
+        } else {
+            acc[evento.category] = evento.price * evento.assistance;
+        }
+        return acc;
+    }, {});
+}
+//estimated attendance by category up
+let past_category_attendance = (array) => {
+    return array.reduce((acc, e) => {
+        if (acc[e.category]) {
+            acc[e.category].assistance += e.assistance;
+            acc[e.category].capacity += e.capacity;
+        } else {
+            acc[e.category] = {
+                assistance: e.assistance,
+                capacity: e.capacity,
+            };
+        }
+        return acc;
+    }, {})
+    
+}
+
+//calc porcentage
+let past_porcentage_attendance = (array_category)=>{
+    let porcentage_attendance = {}
+    for(let el in array_category){
+        porcentage = (array_category[el].assistance/array_category[el].capacity * 100).toFixed(2)
+        porcentage_attendance[el] = parseFloat(porcentage);
+    }
+    return porcentage_attendance;
+}
+
+//unifico los objetos
+let unify_array_info = (profit,assistance)=>{
+    let evento = []
+    for (let el in profit){
+        evento.push({
+            category: el,
+            revenues: profit[el],
+            porcentage:assistance[el]
+        })
+    }
+    return evento
+}
+
 
 
 
